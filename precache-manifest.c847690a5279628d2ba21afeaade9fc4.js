81df3db453f98f69b0477cc9f0be4bb1aae1df91
Ajani Stewart <theamjaha@gmail.com>
2019-07-30 07:53:21 -0400

    Updates

---
+++ precache-manifest.c847690a5279628d2ba21afeaade9fc4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d13003a5c083eaf0336eeecd9eb70d8a",
+    "url": "/bank-of-react/index.html"
+  },
+  {
+    "revision": "7edea89a60677f8a8b82",
+    "url": "/bank-of-react/static/css/main.d08945fa.chunk.css"
+  },
+  {
+    "revision": "c6c97d991627589ca466",
+    "url": "/bank-of-react/static/js/2.342ab72d.chunk.js"
+  },
+  {
+    "revision": "7edea89a60677f8a8b82",
+    "url": "/bank-of-react/static/js/main.f44f2b7a.chunk.js"
+  },
+  {
+    "revision": "3e69701e1bb0d501d040",
+    "url": "/bank-of-react/static/js/runtime~main.6b66607f.js"
+  }
+]);
