e7c0e0b178ac0a828be1f631cab670fada6f0238
Ajani Stewart <theamjaha@gmail.com>
2019-07-31 19:12:27 -0400

    Updates

--- precache-manifest.65d97b5c9ce4a1dbb9fadb0e2213b440.js
+++ precache-manifest.65d97b5c9ce4a1dbb9fadb0e2213b440.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "434c7c51d74857e753d058e7b6c676fb",
+    "revision": "e999dc22fe266f9f3519c297b7da27e8",
     "url": "/bank-of-react/index.html"
   },
   {
-    "revision": "7f78bc006e3cb01cf79c",
+    "revision": "42c71a35efb5df537dd7",
     "url": "/bank-of-react/static/css/main.d08945fa.chunk.css"
   },
   {
-    "revision": "c6c97d991627589ca466",
-    "url": "/bank-of-react/static/js/2.342ab72d.chunk.js"
+    "revision": "5113257e5bf4e5a1fec1",
+    "url": "/bank-of-react/static/js/2.b13d04e1.chunk.js"
   },
   {
-    "revision": "7f78bc006e3cb01cf79c",
-    "url": "/bank-of-react/static/js/main.332189a9.chunk.js"
+    "revision": "42c71a35efb5df537dd7",
+    "url": "/bank-of-react/static/js/main.70738f45.chunk.js"
   },
   {
     "revision": "3e69701e1bb0d501d040",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/bank-of-react/precache-manifest.3d9f6a0727e2014b90bc02844fd712c2.js"
+  "/bank-of-react/precache-manifest.65d97b5c9ce4a1dbb9fadb0e2213b440.js"
 );
 
 self.addEventListener('message', (event) => {
